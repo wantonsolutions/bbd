b102511a09b1584735b960bb841a399d347a977c
wantonsolutions <stewbertgrant@gmail.com>
2024-03-07 12:50:03 -0800

    improved the run script

--- src/run.sh
+++ src/run.sh
@@ -25,12 +25,12 @@ else
     exit 1
 fi
 
-#sync with the build server
-d=`pwd`; rsync -a yak-01.sysnet.ucsd.edu:"$d"/* ./;
+#sync with the build serve
+rsync -a yak-01.sysnet.ucsd.edu:"/usr/local/home/ssgrant/bbd/src/bin/ ./;
 
 ts='taskset -c 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79'
 
         // #define MTU_SIZE 1024
 #run the program
-$ts ./"$program"
+$ts ./bin/"$program"
  
