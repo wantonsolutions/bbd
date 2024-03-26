b2bd9e262478a421f79703f52b927219c4832a1d
Alex <alexliu0809@hotmail.com>
2024-03-26 14:03:59 -0700

    more

--- src/slogger/py/toyc.c
+++ src/slogger/py/toyc.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+const int BUF_SIZE = 1024; 
 int square(int i) {
 	return i * i;
+}
+
+bool write(void *op, int size){
+	char buf[BUF_SIZE];
+	strcpy(buf, op, size);
+	printf("%s\n", buf);
+}
+
+void* read(){
+	char buf[BUF_SIZE];
+	memset(buf, 1, BUF_SIZE * sizeof(buf[0]));
+	return buf
 }
 
