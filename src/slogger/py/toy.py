d369fe5fe1352f89e9cd38c9895b9ba01d71833e
Alex <alexliu0809@hotmail.com>
2024-03-26 16:25:25 -0700

    save

--- src/slogger/py/toy.py
+++ src/slogger/py/toy.py
@@ -1,5 +1,8 @@
 from ctypes import *
 import pickle
+import time
+from functools import partial
+
 so_file = "my_functions.so"
 MY_FUNCTIONS = CDLL(so_file)
 # print(type(my_functions))
@@ -57,7 +60,7 @@ a = []
 # a.append()
 # call_class_method_given_name(a, 'append', 9)
 
-def call_class_method_given_name(class_instance, method, sorted_method_names, vs):
+def call_class_method_given_name(class_instance, method, vs):
 	# the traditional way
 	# print(class_instance, method, vs)
 	# method_get = getattr(class_instance, method)
@@ -72,14 +75,18 @@ def call_class_method_given_name(class_instance, method, sorted_method_names, vs
 
 	method_retrieved, vs_retrieved = pickle.loads(retrieved_payload)
 
-	method_get = getattr(class_instance, method)
+	method_get = getattr(class_instance, "old_" + method)
 
-	method_get(*vs)
+	ret = method_get(*vs)
 
-	print(class_instance)
+	if ret != None:
+		print("class instance: {}, ret of exec: {}".format(class_instance, ret))
+	else:
+		print("class instance: {}".format(class_instance, ret))
+
+	return ret
 	# print(payload_with_method.hex())
 	# print(retrieved_payload.hex())
-
 
 class Add():
 	def __init__(self):
@@ -89,12 +96,77 @@ class Add():
 		return a + b
 
 
+# import time
+# a = Add()
+# stime = time.time()
+# for i in range(0, 1):
+# 	a.add(0, i)
+# etime = time.time()
+# print()
+# # print(etime - stime)
 
-class_name = list
+
+def call_class_method_given_name_wrapper(bbd_class_instance = None, bbd_method_name = None, args = None, kwargs = None):
+	# the traditional way
+	# print(class_instance, method, vs)
+	# method_get = getattr(class_instance, method)
+	# method_get(*vs)
+	# print(class_instance)
+
+	payload_with_method = pickle.dumps((bbd_method_name, args, kwargs), 0)
+	
+	my_bytes_size = take_bytes_and_dump(payload_with_method)
+	
+	retrieved_payload = read_dumped_bytes(my_bytes_size)
+
+	method_retrieved, args, kwargs = pickle.loads(retrieved_payload)
+
+	method_get = getattr(bbd_class_instance, "old_" + bbd_method_name)
+
+	ret = method_get(*args, **kwargs)
+
+	if ret != None:
+		print("class instance: {}, ret of exec: {}".format(bbd_class_instance, ret))
+	else:
+		print("class instance: {}".format(bbd_class_instance, ret))
+
+	return ret
 
 
+def new_func(*args, bbd_class_instance = None, bbd_method_name = None, **kwargs):
+	# print("new_func")
+	# print(args)
+	# print(bbd_class_instance)
+	# print(bbd_method_name)
+	# print(kwargs)
+	# print("new_func ends")
+	return call_class_method_given_name_wrapper(bbd_class_instance = bbd_class_instance, bbd_method_name = bbd_method_name, args=args, kwargs = kwargs)
+	
 
-class_dict = class_name.__dict__
+def craft_func(class_name, method_name, old_func_name, *args, **kwargs):
+	pass
+
+def register_class_method(class_instance, method_name):
+	print('Registering class: {} method: {}'.format(class_instance, method_name))
+	# Craft new function that has the same name as method_name
+	new_func_name = "distributed_{}".format(method_name)
+	# new_func_pointer = 
+	# setattr(class_name, method_name, new_func_name)
+	old_func_pointer = getattr(class_instance, method_name)
+	old_func_name = "old_{}".format(method_name)
+	setattr(class_instance, old_func_name, old_func_pointer)
+
+	# set current
+	p = partial(new_func, bbd_class_instance = class_instance, bbd_method_name = method_name)
+	setattr(class_instance, method_name, p)
+	# getattr(class_instance, method_name)(1,2)
+
+
+CLASS_NAME = Add
+METHOD_NAME = 'add'
+
+
+class_dict = CLASS_NAME.__dict__
 # print(class_dict)
 
 method_names = []
@@ -107,29 +179,26 @@ method_names = sorted(method_names)
 print(
 	'Class Name: {}\n\
 	Class Methods: {}'.format(
-		class_name,
+		CLASS_NAME,
 		method_names
 ))
 
-import time
-a = Add()
-stime = time.time()
-for i in range(0, 1):
-	a.add(0, i)
-etime = time.time()
+instance = CLASS_NAME()
+
+
+register_class_method(instance, METHOD_NAME)
 print()
-# print(etime - stime)
+print('---Finished Registering---\n\n')
 
 
-instance = class_name()
+print('----- Running new add -----')
+res = instance.add(5,10)
 
-stime = time.time()
-for i in range(0, 1):
-	call_class_method_given_name(instance, 'append', method_names, (1,))
-etime = time.time()
-print()
-# print(etime - stime)
+print(res)
+print('----- End Running New Add -----')
 
-
-def register_class_method(class_name, method_name):
-	pass
+# stime = time.time()
+# for i in range(0, 1):
+# 	call_class_method_given_name(instance, METHOD_NAME, (1,6))
+# etime = time.time()
+# print()
